2a5c579474408c4ae021f57b835c0a98a0d18a07
Daniel Adebonojo <dedan2343@gmail.com>
2021-03-05 04:36:39 +0100

    error 2 test client side fixed

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9331,26 +9331,13 @@ var _index = require("./index");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 var busSignup = /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(businessName, fullName, businessEmail, businessPhoneNumber, businessPassword, submitButton) {
-    var res, dataErrors, rawErrorMsg, keys, validationErrorMessage, _i, _Object$entries, _Object$entries$_i, key, value;
-
+    var res;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
@@ -9386,37 +9373,8 @@ var busSignup = /*#__PURE__*/function () {
           case 7:
             _context.prev = 7;
             _context.t0 = _context["catch"](0);
-            console.log(_context.t0.response);
-
-            if (_context.t0.response.data.error._message) {
-              if (_context.t0.response.data.error._message === 'BusinessAccount validation failed' || 'BusinessUser validation failed') {
-                dataErrors = _context.t0.response.data.error.errors;
-                rawErrorMsg = {};
-                keys = Object.keys(dataErrors);
-                keys.forEach(function (key) {
-                  var message = dataErrors[key].message;
-
-                  if (dataErrors[key].properties && dataErrors[key].properties.message) {
-                    message = dataErrors[key].properties.message.replace('`{PATH}`', key);
-                  }
-
-                  message = message.replace('Path ', '').replace(key, '').trim();
-                  rawErrorMsg[key] = message;
-                });
-                validationErrorMessage = [];
-
-                for (_i = 0, _Object$entries = Object.entries(rawErrorMsg); _i < _Object$entries.length; _i++) {
-                  _Object$entries$_i = _slicedToArray(_Object$entries[_i], 2), key = _Object$entries$_i[0], value = _Object$entries$_i[1];
-                  validationErrorMessage.push(value);
-                }
-
-                (0, _alert.showAlert)('error', validationErrorMessage);
-                (0, _index.stopLoadingBtnSpinner)(submitButton);
-              }
-            } else {
-              (0, _alert.showAlert)('error', _context.t0.response.data.message);
-              (0, _index.stopLoadingBtnSpinner)(submitButton);
-            }
+            (0, _alert.showAlert)('error', _context.t0.response.data.message);
+            (0, _index.stopLoadingBtnSpinner)(submitButton);
 
           case 11:
           case "end":
